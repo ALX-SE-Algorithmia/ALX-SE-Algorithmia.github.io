77c50a11de581b2aad4d108f2f0fe661c37045c6
Dohou Daniel Favour <dohoudanielfavour@gmail.com>
2023-10-28 16:15:50 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,41 @@
-$('#header').prepend('<div id="menu-icon"><span class="first"></span><span class="second"></span><span class="third"></span></div>');
-	
-$("#menu-icon").on("click", function(){
-$("nav").slideToggle();
-$(this).toggleClass("active");
+// Intersection observer for the navbar
+const header = document.querySelector("header");
+const section = document.querySelector(".hero-section");
+const sectionOptions = {
+  rootMargin: "-400px 0px 0px 0px",
+};
+
+const sectionObserver = new IntersectionObserver(function (
+  entries,
+  sectionObserver
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOptions);
+
+sectionObserver.observe(section);
+
+//scroll to the top
+function scrolltop() {
+    window.scrollTo(0, 0);
+  }
+
+// menu responsivness
+let menu = document.getElementById("menu-bar");
+let navbar = document.querySelector(".nav-container");
+
+window.onscroll = function () {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+};
+
+menu.addEventListener("click", function () {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
 });
